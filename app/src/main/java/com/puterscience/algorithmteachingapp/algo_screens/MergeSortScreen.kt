c90ca34efab5b30e9577108ea7803683ad64ad4a
alexh <alex_ha_192@protonmail.com>
2024-02-22 15:25:32 +0000

    release candidate 1.a

--- app/src/main/java/com/puterscience/algorithmteachingapp/algo_screens/MergeSortScreen.kt
+++ app/src/main/java/com/puterscience/algorithmteachingapp/algo_screens/MergeSortScreen.kt
@@ -30,7 +30,6 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
